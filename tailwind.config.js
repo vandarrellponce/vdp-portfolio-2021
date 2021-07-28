4cbe067a0ce92dfc9c53540c89c41bc915e9b43b
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-28 13:27:40 +0800

    adding _document.jsx, added link for font, added font family in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      caveat: ['"Caveat", cursive'],
+    },
     extend: {},
   },
   variants: {
