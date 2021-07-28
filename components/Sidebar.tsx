db8c2e64919a28b13c745062ffa8fdc52729ea64
Van Darrell Ponce <riversideriver@yahoo.com>
2021-07-28 11:24:38 +0800

    added sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,18 @@
+const Sidebar = () => {
+  return (
+    <div>
+      <img
+        src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.6435-9/133079395_4003491639663437_7040102776429762782_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeGYAe8PhoX64Bvpp2M_oax39AvjL3xrYQr0C-MvfGthCtsTOV2EP87y4i9FmiMJC5Yp39RfVh96kp-dG_DxEdel&_nc_ohc=skp2sJwp2GAAX-l-bCP&tn=0XpDb6VUv5jaqUUY&_nc_ht=scontent.fceb2-1.fna&oh=4fd3566bce3886c761a201f194795ecf&oe=61266D39"
+        alt="Van Darrell Ponce"
+      />
+
+      <h3>
+        <span>Van Darrell </span>Ponce
+      </h3>
+      <p>Web Developer</p>
+      <p>Download Resume</p>
+    </div>
+  )
+}
+
+export default Sidebar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Sidebar from '../components/Sidebar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6 lg:px-24 my-14">
+      <div className="lg:col-span-3 bg-white rounded-2xl col-span-12 p-4 text-center">
+        <Sidebar />
+      </div>
+      <div className="lg:col-span-9 bg-white rounded-2xl col-span-12">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
