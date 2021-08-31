61708b7e29de8931976f715bb33ebdaed77b2e04
Van Darrell Ponce <riversideriver@yahoo.com>
2021-08-31 22:28:03 +0800

    updated profile pic image

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,12 +7,13 @@ import {
 
 import { GoLocation } from 'react-icons/go'
 import { GiTie } from 'react-icons/gi'
-import Link from 'next/link'
+
+const ProfilePic = '../public/images/darrell.jpg'
 const Sidebar = () => {
   return (
     <div className="px-0 py-0">
       <img
-        src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.6435-9/133079395_4003491639663437_7040102776429762782_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeGYAe8PhoX64Bvpp2M_oax39AvjL3xrYQr0C-MvfGthCtsTOV2EP87y4i9FmiMJC5Yp39RfVh96kp-dG_DxEdel&_nc_ohc=skp2sJwp2GAAX-l-bCP&tn=0XpDb6VUv5jaqUUY&_nc_ht=scontent.fceb2-1.fna&oh=4fd3566bce3886c761a201f194795ecf&oe=61266D39"
+        src={ProfilePic}
         alt="Van Darrell Ponce"
         className="object-contain mx-auto rounded-full w-52 "
       />
